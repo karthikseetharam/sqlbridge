300a50e55ae10692d7f451260edd4fd659af270e
Karthik <karthik.sitharam@gmail.com>
2020-10-19 22:11:59 +0530

    Update OracleCrawlerTest.java

--- oracle-crawler/src/test/java/com/techatpark/scubebird/crawler/oracle/OracleCrawlerTest.java
+++ oracle-crawler/src/test/java/com/techatpark/scubebird/crawler/oracle/OracleCrawlerTest.java
@@ -1,29 +1,28 @@
 package com.techatpark.scubebird.crawler.oracle;
 
+import com.techatpark.scubebird.core.model.DaoProject;
 import com.techatpark.scubebird.core.exception.ScubeException;
+import com.techatpark.scubebird.core.model.Schema;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.Arrays;
 
 class OracleCrawlerTest {
 
     @Test
-    void testGetSchema() throws ScubeException {
-//        DaoProject daoProject = new DaoProject();
-//
-//        daoProject.setName("Sakila");
-//        daoProject.setUrl("jdbc:oracle:thin:@localhost:51521:xe");
-//        daoProject.setUserName("c##chris");
-//        daoProject.setPassword("password");
-//        daoProject.setSchemaName("c##chris");
-//        daoProject.setTablePatterns(List.of("%"));
-//
-//        CrawlerConfig crawlerConfig = new CrawlerConfig();
-//        crawlerConfig.setClassName("com.techatpark.scubebird.crawler.oracle.OracleCrawler");
-//        crawlerConfig.setDriverName("oracle.jdbc.driver.OracleDriver");
-//        crawlerConfig.setName("Oracle");
-//        daoProject.setCrawlerConfig(crawlerConfig);
-//
-//        OracleCrawler oracleCrawler = new OracleCrawler();
-//        Schema schema = oracleCrawler.getSchema(daoProject);
-//        System.out.printf("schema");
+    void testGetSchema() throws ScubeException, SQLException {
+        DaoProject daoProject = new DaoProject();
+
+        daoProject.setName("SMITHJ");
+        daoProject.setUrl("jdbc:oracle:thin:@localhost:51521:xe");
+        daoProject.setUserName("SYSTEM");
+        daoProject.setPassword("test123");
+        daoProject.setSchemaName("SMITHJ");
+        daoProject.setTablePatterns(Arrays.asList("actor","film_actor"));
+
+        OracleCrawler oracleCrawler = new OracleCrawler(daoProject);
+        Schema schema = oracleCrawler.getSchema();
+        System.out.printf("schema");
     }
 }
